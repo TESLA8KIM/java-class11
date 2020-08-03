22a01e3d4586b2e07655eb8e259e5ab7171b91a9
TESLA8KIM <joonkim0414@naver.com>
2020-08-03 18:50:12 +0900

    class

--- src/exam/exam.java
+++ src/exam/exam.java
@@ -12,7 +12,14 @@ import java.util.Scanner;
 public class exam{
 	public static void main(String[] args){
 
-  Scanner sc = new Scanner(System.in);
+		  Scanner sc = new Scanner(System.in);
+		    //String title = ""; //1개의 게시물 밖에 저장하지 못하는 한계점 >> 배열로 change
+		    //String body = "";
+
+		    //<배열 선언> - 복수형으로 이름을 변경한다.
+		    String[] titles = new String[10];
+		    String[] bodies = new String[10];
+		    int lastIndex = 0; //전역변수로 사용해야 한다.
   
   while(true){ 
     System.out.println("명령어를 입력해주세요:");
@@ -24,17 +31,28 @@ public class exam{
       System.out.println("프로그램 종료!");
       break;
     } else if(s1.equals("help")){//help 명령어 추가 및 help 도움말 출력하기
-      System.out.println("exit : 프로그램 종료"); 
+    	System.out.println("exit : 프로그램 종료"); 
+        System.out.println("add : 게시물 등록"); 
+        System.out.println("list : 게시물 목록");  
     } else if(s1.equals("add")){
-      System.out.println("게시물 정보를 저장 : 제목, 내용");
-      String s2 = sc.nextLine();
-      System.out.println(s2);
-    } else if(s1.equals("list")){
-      System.out.println("저장된 게시물 목록 출력");
-      
-     
-      
-    } else{ //주어진 조건이 모두 아니면!
+        System.out.println("제목을 입력해주세요:");
+        titles[lastIndex] = sc.nextLine(); //sc.nextInt()도 가능하다 But nextLine으로 통일
+        //배열 안에 변수명을 사용해야 한다.
+        
+        System.out.println("내용을 입력해주세요:");
+        bodies[lastIndex] = sc.nextLine();
+        //tile과 body에 내용들을 저장했다.
+        lastIndex++; //배열 안 변수 증가하도록 한다.
+
+      } else if(s1.equals("list")){
+        for(int i = 0; i < lastIndex; i++){ //@@for문의 범위는 lastIndex의 개수로 한다.
+          System.out.println("제목: " + titles[i]);
+          System.out.println("내용: " + bodies[i]);
+          System.out.println("====SPACE====");
+        }
+        //위 else if 범위와 지금 else if범위가 다르기 때문에(지역변수 한계점) >> 전역 변수를 사용하도록 한다.
+        
+      } else{ //주어진 조건이 모두 아니면!
       System.out.println("올바른 명령어가 아닙니다.");
     }
   }
